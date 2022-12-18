1c06016da0217dde57d4ee1559109b639fc7b7d6
Justinas <99617300+Adojus@users.noreply.github.com>
2022-12-18 16:36:56 +0200

    Implemented connection to database

---
+++ BlazorServer/Models/PersonModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorServer.Models
+{
+    public class PersonModel
+    {
+        public int id { get; set; }
+        public string vardas { get; set; }
+        public string pavarde { get; set; }
+    }
+}
--- DataLibrary/IDataAccess.cs
+++ DataLibrary/IDataAccess.cs
@@ -2,7 +2,7 @@
 {
     public interface IDataAccess
     {
-        List<T> LoadData<T, U>(string sql, U parameters, string connectionString);
-        void SaveData<T>(string sql, T parameters, string connectionString);
+        Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionString);
+        Task SaveData<T>(string sql, T parameters, string connectionString);
     }
 }
