d20e2bf1b9b5665ee144f124486973b06b85ace3
goblin <gustonas1@gmail.com>
2022-12-18 20:16:33 +0200

    summary
    asd

---
+++ BlazorServer/Models/Naudotojas.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BlazorServer.Models
+{
+    public class Naudotojas
+    {
+        [Required]
+        public string vardas { get; set; }
+        [Required]
+        public string pavarde { get; set; }
+        [Required]
+        public string elPastas { get; set; }
+        public string miestas { get; set; }
+        public int pastoKodas { get; set; }
+        public string adresas { get; set; }
+        [Required]
+        public string slaptazodis { get; set; }
+        public string slapyvardis { get; set; }
+        public string lytis { get; set; }
+        public DateTime registracijosData { get; set; }
+        public string role { get; set; }
+    }
+}
