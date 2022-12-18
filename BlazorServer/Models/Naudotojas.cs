047815c0808db68cbed5a37810fca595ae429d71
Justinas <99617300+Adojus@users.noreply.github.com>
2022-12-19 00:47:52 +0200

    new db

--- BlazorServer/Models/Naudotojas.cs
+++ BlazorServer/Models/Naudotojas.cs
@@ -3,6 +3,7 @@ namespace BlazorServer.Models
 {
     public class Naudotojas
     {
+        public int id { get; set; }
         [Required]
         public string vardas { get; set; }
         [Required]
@@ -18,32 +19,7 @@ namespace BlazorServer.Models
         public string registracijosData { get; set; }
         public string lytis { get; set; }
         public string role { get; set; }
-        public int id { get; set; }
+
     }
-
-
-
-
-    //public class Naudotojas
-    //{
-    //    [Required]
-    //    public string vardas { get; set; }
-    //    [Required]
-    //    public string pavarde { get; set; }
-    //    [Required]
-    //    public string elPastas { get; set; }
-    //    public string miestas { get; set; }
-    //    public int pastoKodas { get; set; }
-    //    public string adresas { get; set; }
-    //    [Required]
-    //    public string slaptazodis { get; set; }
-    //    public string slapyvardis { get; set; }
-    //    public string lytis { get; set; }
-    //    public DateTime registracijosData { get; set; }
-    //    public string role { get; set; }
-    //}
-
-
-
 
 }
