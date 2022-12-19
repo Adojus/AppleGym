0077a7d0cef14843fa96c6af84346bed01ce541d
goblin <gustonas1@gmail.com>
2022-12-19 21:56:06 +0200

    naryte funkcionavimas ir index pakeitimai

---
+++ BlazorServer/Models/Klientas.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorServer.Models
+{
+    public class Klientas
+    {
+        public int id { get; set; }
+        public int narioStatusas { get; set; }
+        public string pazangosLygis { get; set; }
+        public int fk_Naudotojaiid { get; set; }
+    }
+}
