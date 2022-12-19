4a9546a12730915d60a45591285112d84f0292e1
Justinas <99617300+Adojus@users.noreply.github.com>
2022-12-19 19:54:01 +0200

    implemented report generating

---
+++ BlazorServer/Models/AtaskaitaModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorServer.Models
+{
+    public class AtaskaitaModel
+    {
+        public int id { get; set; }
+        public string data { get; set; }
+        public string pavadinimas { get; set; }
+        public string aprasymas { get; set; }
+        public int fk_Administratoriaiid { get; set; }
+    }
+}
