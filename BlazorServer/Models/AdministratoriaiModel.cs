0a60ea6eb86f6304cf476b3aef2092bd0b80082f
Justinas <99617300+Adojus@users.noreply.github.com>
2022-12-19 18:30:44 +0200

    admin can make an announcment

---
+++ BlazorServer/Models/AdministratoriaiModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorServer.Models
+{
+    public class AdministratoriaiModel
+    {
+        public int Id { get; set; }
+        public int naujienuSk { get; set; }
+        public int ataskaituSk { get; set; }
+        public string lygis { get; set; }
+        public int fk_naudotojaiid { get; set; }
+    }
+}
