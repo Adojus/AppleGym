7d2879aa99fdbc1224230832bcf2903be5048afd
Justinas <99617300+Adojus@users.noreply.github.com>
2022-12-19 14:42:26 +0200

    implemented registration form validation

--- BlazorServer/Models/Naudotojas.cs
+++ BlazorServer/Models/Naudotojas.cs
@@ -5,21 +5,41 @@ namespace BlazorServer.Models
     {
         public int id { get; set; }
         [Required]
+        [StringLength(20, ErrorMessage = "Per ilgas vardas.")]
         public string vardas { get; set; }
         [Required]
+        [StringLength(20, ErrorMessage = "Per ilga pavardė.")]
         public string pavarde { get; set; }
         [Required]
+        [StringLength(50, ErrorMessage = "Per ilgas el. paštas.")]
         public string elPastas { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "Per ilgas miestas.")]
         public string miestas { get; set; }
+        [Required]
+        [StringLength(20, ErrorMessage = "Per ilgas pašto kodas.")]
         public string pastoKodas { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "Per ilgas adresas.")]
         public string adresas { get; set; }
         [Required]
+        [StringLength(50, ErrorMessage = "Per ilgas slaptažodis.")]
         public string slaptazodis { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage = "Per ilgas slapyvardis.")]
         public string slapyvardis { get; set; }
         public string registracijosData { get; set; }
+        [Required]
+
         public string lytis { get; set; }
         public string role { get; set; }
 
     }
+
+    //public class Lytys 
+    //{ 
+    //    public readonly string vyras = "vyras";
+    //    public readonly string moteris = "moteris";
+    //}
 
 }
