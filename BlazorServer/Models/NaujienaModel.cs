3bc7b116485887d58d1196cf1084e6bf9382f496
Justinas <99617300+Adojus@users.noreply.github.com>
2022-12-19 19:08:44 +0200

    injects

---
+++ BlazorServer/Models/NaujienaModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorServer.Models
+{
+    public class NaujienaModel
+    {
+        public int id { get; set; }
+        public string data { get; set; }
+        public string tekstas { get; set; }
+        public string antraste { get; set; }
+        public int fk_Administratoriaiid { get; set; }
+    }
+}
